748d512f276423c417e63a131e91156601eceeb0
ElijahKamski <yarikkhripkov@gmail.com>
2019-05-29 16:05:52 +0300

    Added raspberry support

---
+++ PI.py
@@ -0,0 +1,41 @@
+import face_recognition
+import picamera
+import numpy as np
+import pickle
+
+# Get a reference to the Raspberry Pi camera.
+# If this fails, make sure you have a camera connected to the RPi and that you
+# enabled your camera in raspi-config and rebooted first.
+camera = picamera.PiCamera()
+camera.resolution = (320, 240)
+output = np.empty((240, 320, 3), dtype=np.uint8)
+
+with open('dataset_faces.dat', 'rb') as f:
+    Pol_Kan = pickle.load(f)
+users = list(Pol_Kan.keys())
+encodings = [Pol_Kan[kk] for kk in Pol_Kan.keys()]
+pupils = set()
+
+# Initialize some variables
+face_locations = []
+face_encodings = []
+
+while True:
+    print("Capturing image.")
+    # Grab a single frame of video from the RPi camera as a numpy array
+    camera.capture(output, format="rgb")
+
+    # Find all the faces and face encodings in the current frame of video
+    face_locations = face_recognition.face_locations(output)
+    print("Found {} faces in image.".format(len(face_locations)))
+    face_encodings = face_recognition.face_encodings(output, face_locations)
+
+    # Loop over each face found in the frame to see if it's someone we know.
+    for face_encoding in face_encodings:
+        # See if the face is a match for the known face(s)
+        matches = face_recognition.compare_faces(encodings, face_encoding)
+        face_distances = face_recognition.face_distance(encodings, face_encoding)
+        best_match_index = np.argmin(face_distances)
+        if matches[best_match_index]:
+            pupils.add(users[best_match_index])
+    print(pupils)
